7de0e8fbfb978db4b5feda46cd67a229c300c719
wregann <wregann@bu.edu>
2022-06-03 20:22:51 -0400

    Initial Commit 2 (not working yet)

--- table_summarizer_main.py
+++ table_summarizer_main.py
@@ -1,10 +1,77 @@
-from tkinter import *
+import tkinter as tk
+from turtle import back, bgcolor
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+import ts_config as cf
 
-root = Tk()
+class UpperRightStats(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+    def add_onevar_stats(self, data):
+        # Summary Statistics
+        tk.Label(self.parent, font = cf.h2_font, text="Summary Statistics").grid(row=1, column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Mean: " + str(np.mean(data))).grid(row=2, column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Median: " + str(np.median(data))).grid(row=3, column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Std: " + str(np.std(data))).grid(row=4,column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Min: " + str(np.min(data))).grid(row=5,column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Max: " + str(np.max(data))).grid(row=6,column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Q1: " + str(np.quantile(data, 0.25))).grid(row=7,column=2, sticky="W")
+        tk.Label(self.parent, font = cf.body_font, text="Q3: " + str(np.quantile(data, 0.75))).grid(row=8,column=2, sticky="W")
 
-myLabel1 = Label(root, text="Hello!")
-myLabel2 = Label(root, text="Hello2!")
 
-myLabel1.grid(row=5, column=5)
-myLabel2.grid(row=0, column=0)
-root.mainloop()
+class BottomGraphs(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+    def add_onevar_graphs(self, data):
+        fig = plt.figure(figsize = (7, 4), 
+                dpi = 100, facecolor=cf.bg_color)
+        gs = fig.add_gridspec(1,3)
+
+        # Add Histogram
+        histogram_ax = fig.add_subplot(gs[0,0:2])
+        histogram_ax.hist(data)
+        histogram_ax.set_title("Histogram")
+        histogram_ax.set_facecolor(cf.bg_color)
+        whisker_ax = fig.add_subplot(gs[0,2])
+        whisker_ax.boxplot(data)
+        whisker_ax.set_title("Whisker")
+        whisker_ax.set_facecolor(cf.bg_color)
+
+        # plt figure
+        canvas = FigureCanvasTkAgg(fig, master=self.parent)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=20, column=0, columnspan=3) # row 20 was arbitrary
+
+
+class MainApp(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.parent.geometry(cf.window_geom)
+        self.parent.configure(background=cf.bg_color)
+        self.parent.title("Table Summarizer")
+        tk.Label(root, font = cf.h1_font, text="Table Summarizer").grid(row=0, column=0, columnspan=3)
+        tk.Button(root, text='Find Table', font=cf.body_font).grid(row=1, column=0)
+
+        self.upper_right_stats = UpperRightStats(self.parent)
+        self.bottom_graphs = BottomGraphs(self.parent)
+
+        x = np.random.normal(loc=0, scale=25, size=(500))
+        self.upper_right_stats.add_onevar_stats(x)
+        self.bottom_graphs.add_onevar_graphs(x)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApp(root)
+    root.mainloop()
+
+# Matplotlib Tool Bar
+# toolbar = NavigationToolbar2Tk(canvas, root)
+# toolbar.update()
+# canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+
---
+++ ts_config.py
@@ -0,0 +1,5 @@
+h1_font = ("Times", 30, 'bold', 'underline')
+h2_font = ("Times", 25, 'bold')
+body_font = ("Times", 20)
+bg_color = "lightgrey"
+window_geom = "700x700"
